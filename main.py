d36b2f53063788721a7f10f6c8599441ff714805
RuvejdaHamza <ruvejda.hamza@student.uni-pr.edu>
2026-04-15 21:52:26 -0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Main entry point për Klientin - Grupi 33
+Rrjetat Kompjuterike 2025/26
+"""
+
+import sys
+import os
+
+# Shto folderin 'client' në path që Python të gjejë module-t
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'client'))
+
+from client.config import SERVER_HOST, SERVER_PORT, CONNECTION_TIMEOUT
+from client.commands import CommandHandler
+
+import socket
+import json
