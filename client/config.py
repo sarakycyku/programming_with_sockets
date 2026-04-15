0b6c6ecdf90dc0952c210df02acc162bfe624379
Rumesa <rumesa2005@gmail.com>
2026-04-15 22:21:42 +0200

    set server host, ports and timeout

---
+++ client/config.py
@@ -0,0 +1,10 @@
+# Konfigurimet e serverit - Grupi 33
+import os
+
+# IP dhe Porti i serverit kryesor (TCP Socket)
+SERVER_HOST = "192.168.0.17"
+SERVER_PORT = 5000       # Porti kryesor për TCP socket
+CONNECTION_TIMEOUT = 10
+# HTTP Server (për statistika)
+HTTP_HOST = "0.0.0.0"
+HTTP_PORT = 8080
